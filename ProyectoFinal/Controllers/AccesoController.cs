f2597688f88490c792ec44ff6883322f0c2244e6
lucionigro <lucionigrot@gmail.com>
2022-12-20 16:26:32 -0300

    LOGIN EN PROCESO

---
+++ ProyectoFinal/Controllers/AccesoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinal.Models;
+using ProyectoFinal.Datos;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ProyectoFinal.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+    [HttpPost]
+
+    public IActionResult Login(Clientes oclientes)
+    {
+        var oLista = new List<Clientes>();
+        var cn = new Conexion();
+        using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+        {
+            conexion.Open();
+            SqlCommand cmd = new SqlCommand("sp_validar_usuario", conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            using (var dr = cmd.ExecuteReader())
+            {
+                while (dr.Read())
+                    oLista.Add(new Clientes()
+                    {
+                        CORREO = dr["CORREO"].ToString(),
+                        CONTRASENIA = dr["CONTRASENIA"].ToString(),
+                    });
+            }
+
+        }
+                                                                                            
+    }
+}
--- ProyectoFinal/Program.cs
+++ ProyectoFinal/Program.cs
@@ -19,6 +19,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Acceso}/{action=Login}/{id?}");
 
 app.Run();
